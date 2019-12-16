7d76c6b9680cb5f892a99f31e53fe4be163714fb
Pio Nguyễn <50920220+Greeny167@users.noreply.github.com>
2019-12-16 17:28:31 +0700

    commit

--- admin/modules/class/addStdInClass.php
+++ admin/modules/class/addStdInClass.php
@@ -34,15 +34,7 @@ $result1 = $conn->query($sql1);
         $i = 0;
         foreach ($result1 as $row1) {
             ?>
-            <tr>
-                <td>
-                    <input type="checkbox" name="username" id="<?php echo $row1['USERNAME'] ?>">
-                </td>
-                <td><?php echo $row1['USERNAME'] ?></td>
-                <td><?php echo $row1['FULLNAME'] ?></td>
-            </tr>
-            <?php
-            ?>
+            
             <tr>
                 <td>
                     <input type="checkbox" name="username" id="<?php echo $row1['USERNAME'] ?>">
