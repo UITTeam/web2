73e7e82d96ab2ddf7acbc19adf88f47b72fddc13
Mai Như Ngọc <mnngoc@users.noreply.github.com>
2019-12-19 11:15:45 +0700

    edit

--- admin/modules/teacher/controlTeacher.php
+++ admin/modules/teacher/controlTeacher.php
@@ -2,6 +2,9 @@
     include('../connect.php');
       $id = $_GET['id'];
       
+     
+    if (isset($_POST['btnSubmitAdd'])) 
+    {
       $acc_pass = $_POST['txtPassword'];
       $acc_avt = $_FILES['fileAvt']['name'];
       $acc_avt_tmp = $_FILES['fileAvt']['tmp_name'];
@@ -13,8 +16,6 @@
       $sdt_addr = $_POST['txtAddr'];
       $sdt_phone = $_POST['txtPhone'];
       $sdt_certi = $_POST['txtCerti'];
-    if (isset($_POST['btnSubmitAdd'])) 
-    {
       $acc_date = $_POST['txtCreateDate'];
       $acc_usename = $_POST['txtAccount'];
       $sql1 = "INSERT INTO `account`(`username`, `PASS`, `role_id`,`create_date`, `AVATAR`) 
@@ -49,7 +50,20 @@
             }
     }
     else if (isset($_POST['btnSubmitEdit']))
-    {   
+    { 
+      $acc_pass = $_POST['txtPassword'];
+      $acc_avt = $_FILES['fileAvt']['name'];
+      $acc_avt_tmp = $_FILES['fileAvt']['tmp_name'];
+      move_uploaded_file($acc_avt_tmp, '../uploads/'.$acc_avt);
+      $sdt_fullname = $_POST['txtFullname'];
+      $sdt_dob = $_POST['txtDob'];
+      $sdt_sex = $_POST['rbSex'];
+      $sdt_email = $_POST['txtEmail'];
+      $sdt_addr = $_POST['txtAddr'];
+      $sdt_phone = $_POST['txtPhone'];
+      $sdt_certi = $_POST['txtCerti'];
+      $acc_date = $_POST['txtCreateDate'];
+      $acc_usename = $_POST['txtAccount'];  
       if ($acc_avt != '')
         {
             $sql1 = "UPDATE `account` SET `PASS` = '$acc_pass', 
@@ -82,22 +96,48 @@
     }
     else
     {
+      $sql0 = "DELETE FROM `test` WHERE USERNAME='$id'";
+      $sql00 = "DELETE FROM `class` WHERE TEACHER='$id'";
       $sql1 = "DELETE FROM `teacher` WHERE USERNAME='$id'";
       $sql2 = "DELETE FROM `account` WHERE username='$id'";
-      if (mysqli_query($conn, $sql1)) 
+      if (mysqli_query($conn, $sql00))
       {
-         if (mysqli_query($conn, $sql2)) 
+         if (mysqli_query($conn, $sql0))
+         {
+            if (mysqli_query($conn, $sql1)) 
             {
-               header('location:../../index.php?click=teacher&id=1');
-            } 
-            else 
-               {
-               echo "Error: " . $sql2 . "<br>" . mysqli_error($conn);
-          }
-      }
-      else 
-            {
-            echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
+               if (mysqli_query($conn, $sql2)) 
+                  {
+                     header('location:../../index.php?click=teacher&id=1');
+                  } 
+                  else 
+                     {
+                     echo "Error: " . $sql2 . "<br>" . mysqli_error($conn);
+                }
             }
+            else 
+                  {
+                  echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
+                  }
+         }
+         else
+         {
+            echo "Error: " . $sql0 . "<br>" . mysqli_error($conn);
+         }
+      }
+      else
+      {
+         //echo "Error: " . $sql00 . "<br>" . mysqli_error($conn);
+       
+         ?>
+   <script>
+      alert ("Khong xoa duoc do hien tai giao vien dang phu trach lop");
+      window.location = "../../index.php?click=teacher";
+   </script>
+<?php
+   
+      }
+     
+     
     }
 ?>
